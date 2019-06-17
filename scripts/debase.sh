bf79592689ae51b4bc208a5021261f422fbbbdfd
matianfu <matianfu@gmail.com>
2019-06-17 14:05:44 +0800

    add zram-tools

--- scripts/debase.sh
+++ scripts/debase.sh
@@ -3,11 +3,11 @@
 # debootstrap, binfmt-support, qemu-user-static
 
 # tzdata already in base
-INCS+=locales,nano,vim,ifupdown,net-tools,
+INCS+=locales,nano,ifupdown,net-tools,zram-tools,
 INCS+=initramfs-tools,u-boot-tools,btrfs-progs,wireless-tools,i2c-tools,
 INCS+=bluez,bluez-tools,bluetooth,
 INCS+=openssh-server,network-manager,
-INCS+=avahi-daemon,avahi-utils,
+# INCS+=avahi-daemon,avahi-utils,
 # INCS+=samba,rsyslog
 INCS+=libimage-exiftool-perl,imagemagick,ffmpeg
 
