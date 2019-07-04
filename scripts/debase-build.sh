aa4d152eb5f516456ce571d05f10fabe8492a2e4
matianfu <matianfu@gmail.com>
2019-07-04 12:11:46 +0800

    wip

--- scripts/debase-build.sh
+++ scripts/debase-build.sh
@@ -5,16 +5,21 @@
 set -e
 
 SCRIPT_DIR=$(dirname "$0")
+SCROPT_NAME=$(basename "$0")
+ECHO="echo ${SCRIPT_NAME}: "
 
 source $SCRIPT_DIR/main.env
 
-if [ -f $CACHE/$DEBASE_BUILD_TAR ]; then exit; fi
+if [ -f $CACHE/$DEBASE_BUILD_TAR ]; then 
+  $ECHO "$CACHE/$DEBASE_BUILD_TAR exists, skip building"
+  exit 0
+fi
 
-DEBASE=$TMP/debase-build
+WORKDIR=$TMP/debase-build
 
-rm -rf $DEBASE
-mkdir -p $DEBASE
-debootstrap --arch=arm64 --foreign --include=python2.7 --variant=buildd buster $DEBASE
-cp -av /usr/bin/qemu-aarch64-static $DEBASE/usr/bin
-chroot $DEBASE /bin/bash -c "LANG=C /debootstrap/debootstrap --second-stage"
-tar czf $CACHE/$DEBASE_BUILD -C $DEBASE .
+rm -rf $WORKDIR
+mkdir -p $WORKDIR
+debootstrap --arch=arm64 --foreign --include=python2.7 --variant=buildd buster $WORKDIR
+cp -av /usr/bin/qemu-aarch64-static $WORKDIR/usr/bin
+chroot $WORKDIR /bin/bash -c "LANG=C /debootstrap/debootstrap --second-stage"
+tar czf $CACHE/$DEBASE_BUILD_TAR -C $WORKDIR .
--- scripts/debase.sh
+++ scripts/debase.sh
@@ -8,11 +8,12 @@ set -e
 
 SCRIPT_DIR=$(dirname "$0")
 SCRIPT_NAME=$(basename "$0")
+ECHO="echo ${SCRIPT_NAME}: "
 
 source $SCRIPT_DIR/main.env
 
 if [ -f $CACHE/$DEBASE_TAR ]; then
-  echo "[${SCRIPT_NAME}] $CACHE/$DEBASE_TAR exists, skip building."
+  $ECHO "$CACHE/$DEBASE_TAR exists, skip building."
   exit 0
 fi
 
@@ -36,4 +37,4 @@ cp -av /usr/bin/qemu-aarch64-static $WORKDIR/usr/bin
 chroot $WORKDIR /bin/bash -c "LANG=C /debootstrap/debootstrap --second-stage"
 
 tar czf $CACHE/$DEBASE_TAR -C $WORKDIR .
-echo "[${SCRIPT_NAME}] $CACHE/$DEBASE_TAR is ready."
+$ECHO "$CACHE/$DEBASE_TAR is ready."
