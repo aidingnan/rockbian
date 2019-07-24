61cedcb6654447aa93afae74738979bfdb94bc85
matianfu <matianfu@gmail.com>
2019-07-24 12:00:56 +0800

    add uuidgen

--- scripts/debase.sh
+++ scripts/debase.sh
@@ -23,7 +23,7 @@ $ECHO "building $DEBASE_TAR ..."
 
 DIR=$TMP/debase
 
-INCS+=locales,tzdata,initramfs-tools,u-boot-tools,ca-certificates,haveged,zstd,isc-dhcp-client,isc-dhcp-server,ifupdown,
+INCS+=locales,tzdata,initramfs-tools,u-boot-tools,ca-certificates,haveged,zstd,isc-dhcp-client,isc-dhcp-server,ifupdown,uuidgen,
 INCS+=btrfs-progs,i2c-tools,zram-tools,xz-utils,parted,openssl,rfkill,
 INCS+=nano,curl,wget,file,unzip,iputils-ping,ethtool,
 INCS+=net-tools,wireless-tools,network-manager,
