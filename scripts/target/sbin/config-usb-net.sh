5bb7ca08d9aefc539e63bbc780faad0ca2854228
matianfu <matianfu@gmail.com>
2019-07-24 21:27:40 +0800

    fix connection file permission

--- scripts/target/sbin/config-usb-net.sh
+++ scripts/target/sbin/config-usb-net.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-source /run/cowroot/root/data/init/usn
+USN=$(cat /run/cowroot/root/data/init/usn)
 
 D3=${USN:0:2}
 [[ "${USN:2:1}" = 0 ]] && D4=${USN:3:1} || D4=${USN:2:2}
 
-cat > /etc/NetworkManager/system-connections/usb0.nmconnection << EOF
+FILE=/etc/NetworkManager/system-connections/usb0.nmconnection
+
+cat > $FILE  << EOF
 [connection]
 id=usb0
 uuid=9285a52f-6e92-4e5c-83bb-77443ceb64dd
@@ -26,3 +28,6 @@ addr-gen-mode=eui64
 dns-search=
 method=link-local
 EOF
+
+chmod 600 $FILE
+
