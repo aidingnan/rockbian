980740ce3e2238d974899cde7440280d73fdc3d8
matianfu <matianfu@gmail.com>
2019-08-12 16:00:06 +0800

    add domain

--- scripts/mk-vol.sh
+++ scripts/mk-vol.sh
@@ -42,6 +42,10 @@ mkdir -p $MNT/boot
 mkdir -p $MNT/vols
 mkdir -p $MNT/refs/tags
 
+# set domain
+mkdir -p $MNT/data
+echo "aws-cn" > /$MNT/data/domain
+
 # set engineering mode
 mkdir -p $MNT/data/root.0
 touch $MNT/data/root.0/engineering
